c622402e32f07ca3ecd85142e333b1781f5d1412
İrem Kurt <iremkurt19@gmail.com>
2022-04-24 17:48:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName = 'BOB'
+    return customerName;
+}
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'irem'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'zeynep'
+}
+
+
+//upperCaseCustomerName(bob)
